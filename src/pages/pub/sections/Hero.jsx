253d8c00aeec561c756ec2510638237c1cdd843b
Irfan <irfankhan01066@gmial.com>
2026-08-21 11:49:51 +0530

    update button

--- src/pages/pub/sections/Hero.jsx
+++ src/pages/pub/sections/Hero.jsx
@@ -92,7 +92,7 @@ export default function Hero() {
           <Button
             size="lg"
             variant="outline"
-            href="https://github.com/khanIrfank/MeetSphere/releases/download/v1.0.1/MeetSphere.3.apk"
+            href="https://github.com/khanIrfank/MeetSphere/releases/download/v1.0.4/MeetSphere.4.apk"
             target="_blank"
             rel="noopener noreferrer"
             icon={Download}
